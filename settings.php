b132102f994823db2ae9bc69c43e29cf4a3b974e
neswill1 <nenadkonatar@live.ac.me>
2015-04-20 13:47:31 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,47 @@
+<?php
+$adresa[0]=" 900 National Parkway Suite #150"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[0]="Schaumburg, IL 60173";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[0]="Chicago";
+$telephone[0]=8474810400;// Broj koji se poziva na dugme CALL US
+
+$adresa[1]="11890 Montgomery Road Cincinnati, OH 45249"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[1]="Cincinnati, OH 45249";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[1]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[1]="Cincinnati";
+$telephone[1]=5136836600;// Broj koji se poziva na dugme CALL US
+
+$adresa[2]="3760 Kilroy Airport Way Long Beach, CA 90806"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[2]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[2]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[2]="Costa Mesa";
+$telephone[2]=8008439314;// Broj koji se poziva na dugme CALL US
+
+$adresa[3]=" 433 Hackensack Avenue Hackensack, NJ 07601"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[3]="Hackensack, NJ 07601";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[3]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[3]="Hackensack";
+$telephone[3]=2014885888;// Broj koji se poziva na dugme CALL US
+
+$adresa[4]="3760 Kilroy Airport Way Suite 100 Long Beach, CA 90806"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[4]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[4]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[4]="Long Beach";
+$telephone[4]=8003083575;// Broj koji se poziva na dugme CALL US
+
+$adresa[5]="11175 Santa Monica Blvd Suite 700 Los Angeles, CA 90025"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[5]="Los Angeles, CA 90025";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[5]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[5]="Los Angeles";
+$telephone[5]=8008821525;// Broj koji se poziva na dugme CALL US
+
+$adresa[6]="60 Columbia Road Morristown, NJ 07960"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[6]="Morristown, NJ 07960";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[6]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[6]="Morristown";
+$telephone[6]=973-267-7060;// Broj koji se poziva na dugme CALL US
+
+$companynumber=7;
+$theme="b";//Thema koja se aplicira na sve
+$transitionefect="turn"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup", "turn", "slidefade", "flow"
+?>
